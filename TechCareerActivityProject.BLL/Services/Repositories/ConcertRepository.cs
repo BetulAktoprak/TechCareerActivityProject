e9ee2e15b0445cc130d2fe9f7999ccaf0ca61954
Betül aktoprak <betulaktoprak112@gmail.com>
2024-02-05 15:48:45 +0300

    Add project files.

---
+++ TechCareerActivityProject.BLL/Services/Repositories/ConcertRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechCareerActivityProject.BLL.Services.Interfaces;
+using TechCareerActivityProject.DAL.Models;
+
+namespace TechCareerActivityProject.BLL.Services.Repositories
+{
+    public class ConcertRepository : GenericRepository<Concert>, IConcertRepository
+    {
+    }
+}
---
+++ TechCareerActivityProject.DAL/Models/Concert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechCareerActivityProject.DAL.Models
+{
+    public class Concert : BaseModel
+    {
+        public string ConcertName { get; set; }
+        public string ConcertType { get; set; }
+        public string ConcertLocation { get; set; }
+        public string ConcertImage { get; set; }
+    }
+}
